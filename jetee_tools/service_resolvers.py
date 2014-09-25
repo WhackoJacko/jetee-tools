dede4da263d84a94584c77ba8464f27bcb0b9027
Сергей Дубинин <whackojacko@Mac-mini-Sergej.local>
2014-09-25 15:17:54 +0600

    resolvers interface refactored

--- jetee_tools/service_resolvers.py
+++ jetee_tools/service_resolvers.py
@@ -5,17 +5,19 @@ class JeteeServiceConfigResolver(object):
     dns_server = '172.17.42.1'
     service_ip = u'172.17.42.1'
     protocol = u''
+    port = None
 
     def get_real_host(self):
         return u'{}.service.consul'.format(self.host)
 
     def __init__(self, host):
         self.host = host
+        self.resolve_port()
 
-    def render(self, port):
+    def as_string(self):
         raise NotImplementedError
 
-    def resolve(self):
+    def resolve_port(self):
         port = None
         srv_req = DNS.Request(qtype='srv', server=self.dns_server)
         srv_result = srv_req.req(self.get_real_host())
@@ -23,15 +25,16 @@ class JeteeServiceConfigResolver(object):
             if result['typename'] == 'SRV':
                 port = result[u'data'][2]
         assert port is not None
-        return self.render(port)
+        self.port = port
 
 
 class DjangoDatabaseJeteeServiceConfigResolver(JeteeServiceConfigResolver):
     def __init__(self, host, protocol):
         self.protocol = protocol
         self.host = host
+        self.resolve_port()
 
-    def render(self, port):
+    def as_string(self):
         return {
             'ENGINE': 'django.db.backends.{}'.format(self.protocol),
             # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -39,7 +42,7 @@ class DjangoDatabaseJeteeServiceConfigResolver(JeteeServiceConfigResolver):
             'USER': 'docker',  # Not used with sqlite3.
             'PASSWORD': u'docker',  # Not used with sqlite3.
             'HOST': self.service_ip,  # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': port,  # Set to empty string for default. Not used with sqlite3.
+            'PORT': self.port,  # Set to empty string for default. Not used with sqlite3.
         }
 
 
@@ -49,6 +52,7 @@ class RedisJeteeServiceConfigResolver(JeteeServiceConfigResolver):
     def __init__(self, host, db=0):
         self.db = db
         self.host = host
+        self.resolve_port()
 
-    def render(self, port):
-        return u'redis://{}:{}/{}'.format(self.service_ip, port, self.db)
+    def as_string(self):
+        return u'redis://{}:{}/{}'.format(self.service_ip, self.port, self.db)
